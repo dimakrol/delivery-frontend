1019d729c0f0b1b8ab1dd7f75faa00c27e637ba9
dimakrol <dmitriy.krol.v@gmail.com>
2021-08-04 00:30:29 +0300

    Use apollo reactive vars

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,7 @@
 import React from 'react';
+import {isLoggedInVar} from "../apollo";
 
-export const LoggedInRouter = () => <span>Logged Out</span>;
+export const LoggedInRouter = () => <div>
+  <h1>Logged In</h1>
+  <button onClick={() => isLoggedInVar(false)}>Click to logout</button>
+</div>;
