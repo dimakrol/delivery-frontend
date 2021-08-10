32196e287603178454b83fece46c368f460aae7a
dimakrol <dmitriy.krol.v@gmail.com>
2021-08-10 18:32:24 +0300

    added router types, added some routes.

---
+++ src/pages/login.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export const Login = () => {
+  return <span>Login</span>
+}
+//
+// import React from 'react';
+// import {useForm} from "react-hook-form";
+//
+// interface IForm {
+//   email: string,
+//   password: string,
+// }
+//
+// export const LoggedOutRouter = () => {
+//   const { register, watch, handleSubmit, formState: { errors } } = useForm<IForm>();
+//
+//   const onSubmit = () => {
+//     console.log(watch())
+//   }
+//   const onInvalid = () => {
+//     console.log('not valid ', errors)
+//   }
+//   return (
+//     <div>
+//       <h1>Logged Out</h1>
+//       <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+//         <div>
+//           <input
+//             {...register('email', {
+//               required: 'this is required',
+//               validate: (email:string) => email.includes('@gmail.com')})}
+//             type="email"
+//             placeholder="email"
+//           />
+//           {errors.email?.message && <span className="font-bold text-red-600">{errors.email?.message}</span>}
+//           {errors.email?.type === 'validate' && <span className="font-bold text-red-600">Only Gmail</span>}
+//         </div>
+//         <div>
+//           <input
+//             {...register('password', {required: true})}
+//             type="password"
+//             placeholder="password"
+//           />
+//         </div>
+//         <button className="bg-yellow-300 text-white">Login</button>
+//       </form>
+//     </div>
+//   );
+// }
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,44 +1,17 @@
 import React from 'react';
-import {useForm} from "react-hook-form";
-
-interface IForm {
-  email: string,
-  password: string,
-}
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Login} from "../pages/login";
+import {CreateAccount} from "../pages/create-account";
 
 export const LoggedOutRouter = () => {
-  const { register, watch, handleSubmit, formState: { errors } } = useForm<IForm>();
-
-  const onSubmit = () => {
-    console.log(watch())
-  }
-  const onInvalid = () => {
-    console.log('not valid ', errors)
-  }
-  return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            {...register('email', {
-              required: 'this is required',
-              validate: (email:string) => email.includes('@gmail.com')})}
-            type="email"
-            placeholder="email"
-          />
-          {errors.email?.message && <span className="font-bold text-red-600">{errors.email?.message}</span>}
-          {errors.email?.type === 'validate' && <span className="font-bold text-red-600">Only Gmail</span>}
-        </div>
-        <div>
-          <input
-            {...register('password', {required: true})}
-            type="password"
-            placeholder="password"
-          />
-        </div>
-        <button className="bg-yellow-300 text-white">Login</button>
-      </form>
-    </div>
-  );
+  return <Router>
+    <Switch>
+      <Route path="/create-account">
+        <CreateAccount/>
+      </Route>
+      <Route path="/">
+        <Login/>
+      </Route>
+    </Switch>
+  </Router>
 }
