c736aa51ddbbc9cc1d427f01a62101ecdde05f83
dimakrol <dmitriy.krol.v@gmail.com>
2021-08-11 18:32:46 +0300

    Styling login page.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,6 +3,7 @@ import {useForm} from "react-hook-form";
 import {FormError} from "../components/form-error";
 import {gql, useMutation} from "@apollo/client";
 import {LoginMutation, LoginMutationVariables} from "../__generated__/LoginMutation";
+import logo from '../images/logo.svg';
 
 const LOGIN_MUTATION = gql`
   mutation LoginMutation($loginInput: LoginInput!) {
@@ -48,33 +49,35 @@ export const Login = () => {
     }
   }
 
-  return (<span className="h-screen flex items-center justify-center bg-gray-800">
-    <div className="bg-white w-full max-w-lg py-7 rounded-lg text-center">
-      <h3 className="text-3xl text-gray-800">Log In</h3>
-      <form onSubmit={handleSubmit(onSubmit)} className="grid gap-2 mt-5 px-5">
-        <input { ...register('email', { required: 'Email is required' }) }
-               type="email"
-               required
-               placeholder="Email"
-               className="input mb-3"
-        />
-        {errors.email?.message && (
-          <FormError errorMessage={errors.email?.message}/>
-        )}
-        <input { ...register('password', { required: 'Password is required', minLength: 6 }) }
-               type="password"
-               required
-               placeholder="Password"
-               className="input mb-3"/>
-        {errors.password?.type === 'minLength' && (
-          <FormError errorMessage="Password must be more than 6 chars."/>
-        )}
-        {errors.password?.message && (
-          <FormError errorMessage={errors.password?.message}/>
-        )}
-        <button className="btn">{loading ? 'Loading...' : 'Log In' }</button>
-        {loginMutationResult?.login.error && <FormError errorMessage={loginMutationResult.login.error}/>}
-      </form>
-    </div>
-  </span>)
+  return (
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <div className='w-full max-w-screen-sm flex flex-col px-5 items-center'>
+        <img src={logo} className='w-52 mb-10' alt="logo"/>
+        <h4 className='w-full font-medium text-left text-3xl mb-5'>Welcome back</h4>
+        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-2 mt-5 w-full">
+          <input { ...register('email', { required: 'Email is required' }) }
+                 type="email"
+                 required
+                 placeholder="Email"
+                 className="input"
+          />
+          {errors.email?.message && (
+            <FormError errorMessage={errors.email?.message}/>
+          )}
+          <input { ...register('password', { required: 'Password is required', minLength: 6 }) }
+                 type="password"
+                 required
+                 placeholder="Password"
+                 className="input"/>
+          {errors.password?.type === 'minLength' && (
+            <FormError errorMessage="Password must be more than 6 chars."/>
+          )}
+          {errors.password?.message && (
+            <FormError errorMessage={errors.password?.message}/>
+          )}
+          <button className="btn">{loading ? 'Loading...' : 'Log In' }</button>
+          {loginMutationResult?.login.error && <FormError errorMessage={loginMutationResult.login.error}/>}
+        </form>
+      </div>
+    </div>)
 }
